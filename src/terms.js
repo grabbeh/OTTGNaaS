454c0b47515a63dc4f3ada5b646d56f153050523
Michael <mbg@outlook.com>
2017-07-03 16:08:59 +0100

    Update terms.js

--- src/terms.js
+++ src/terms.js
@@ -2,11 +2,17 @@ const superagent = require('superagent')
 const unfluff = require('unfluff')
 const url = 'https://www.google.com/policies/privacy/'
 
-const start = async function () {
+const start = async function (x) {
   let res = await superagent.get(url)
   let data = unfluff(res.text)
   let arr = data.text.split('.')
-  return arr
+  return returnSlice(x, arr)
 }
 
 start()
+
+function returnSlice(x, arr){
+  var potentialStartPoints = arr.length - x
+  var randomStartPoint = Math.floor(potentialStartPoints * Math.random())
+  return a.slice(randomStartPoint, randomStartPoint + x)
+}
