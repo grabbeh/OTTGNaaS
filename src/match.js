3d58ad28cffde761b912f5477a90e482846637d7
Michael <mbg@outlook.com>
2017-07-04 17:52:54 +0100

    Update match.js

--- src/match.js
+++ src/match.js
@@ -3,48 +3,50 @@ const _ require('underscore')
 const helper = require('./helper')
 
 exports.matchTextLengthToArea = (speech, terms) => {
-
-let textAreas = terms.map(t, i => {
-   let textBox = new fabric.Textbox(text, {
-          fontSize: 5
+   let textAreas = terms.map(t => {
+      let textBox = new fabric.Textbox(text, {
+             fontSize: 5
+      })
+      let height = textBox.getBoundingRectHeight()  
+      let width = textBox.getBoundingRectWidth()
+      let textArea = height * width
+      return { 
+       text: t,
+       area: textArea
+      }
    })
-   let height = textBox.getBoundingRectHeight()  
-   let width = textBox.getBoundingRectWidth()
-   let textArea = height * width
-   return { 
-    text: t,
-    textId: i,
-    area: textArea
-   }
-})
 
-let speechAreas = speech.map(s, i => {
-  let area = helper.getArea(a)
-  s.area = area
-  s.speechId = i
-  return s
-})
+   let speechAreas = speech.map(s => {
+     return {
+       area: helper.getArea(area), 
+       coordinates: s,
+       width: helper.getWidth(d),
+       topLeft: helper.getTopLeft(d)
+     }
+   })
+   return combine(speechAreas, textAreas)
+}
 
+const combine = (s, t) => { 
+   t.sort(function(a, b) { return a.area - b.area })
+   s.sort(function(a, b) { return a.area - b.area })
+   s.forEach(speech, i => { speech.text = textAreas[i] })
+   return s
+}
+
+/*
 let arr = []
 arr.push(textAreas)
 arr.push(speechAreas)
 let flat = _.flatten(arr, true)
 let vectors = flat.map(f => {
    return [f.area]
-})
-
-
-flat.sort(function(a, b) {
-  return a.area - b.area
-})
-console.log(flat)
-   
+})*/
+ 
 //let clusterResult = cluster(vectors)
 //let matchResult = matchBack(speechAreas, textAreas, clusterResult)
-
-
    
-function matchBack(speechAreas, textAreas, clusterResult){
+const matchBack = (speechAreas, textAreas, clusterResult) => {
   let arr = []
   speechAreas.forEach(s => {
     textAreas.forEach(t => {
@@ -62,24 +64,10 @@ function matchBack(speechAreas, textAreas, clusterResult){
   })
   return clusterResult
 }
-  
-  
 
-//return arr of objects with [{ coords: [], terms: "" }]
-
-function cluster(vectors){
+const cluster = (vectors) => {
   kmeans.clusterize(vectors, {k: speech.length }, (err,res) => {
     if (err) console.error(err)
     else return res
-  })
-}
-   
- let combinedData = fullArr.map((s, i) => {
-    return {
-      coordinates: s.coords,
-      terms: s.terms,
-      width: helper.getWidth(s.coords),
-      topLeft: helper.getTopLeft(s.coords)
-    }
   })
 }
