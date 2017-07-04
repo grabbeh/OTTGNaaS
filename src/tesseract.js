faebe874d2fe4b79b12664d2778ca8d7bb000ce2
Michael <mbg@outlook.com>
2017-07-04 12:16:05 +0100

    Update tesseract.js

--- src/tesseract.js
+++ src/tesseract.js
@@ -1,9 +1,19 @@
-const superagent = require('superagent')
 const Tesseract = require('tesseract')
-
+const request = require('request')
+const fs = require('fs')
+const url = 'http://tesseract.projectnaptha.com/img/eng_bw.png'
+const fileName = 'pic.png'
+const writeFile = fs.createWriteStream(filename)
+ 
 module.exports = async function(url) =>{
-  let image = await superagent(url)
-  let result = await Tesseract.recognize(image)
-  return result
+  request(url).pipe(writeFile).on('close', function() {
+    let results = Tesseract.recognize(fileName)
+    console.log(results)  
+  })
 }
+
+
+
+
+
 
