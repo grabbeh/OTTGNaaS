849c458ae0ba08e2b7570d6ec9edcde60e5a9fd3
Michael <mbg@outlook.com>
2017-07-04 11:23:53 +0100

    Update tesseract.js

--- src/tesseract.js
+++ src/tesseract.js
@@ -1,1 +1,9 @@
+const superagent = require('superagent')
+const Tesseract = require('tesseract')
+
+module.exports = async function(url) =>{
+  let image = await superagent(url)
+  let result = await Tesseract.recognize(image)
+  console.log(result)
+}
 
