8d297eaebb8fad636616e10cf7a8fec33b229b84
Michael <mbg@outlook.com>
2017-07-04 16:34:16 +0100

    Update match.js

--- src/match.js
+++ src/match.js
@@ -1,3 +1,5 @@
+const kmeans = require('node-kmeans')
+const _ require('underscore')
 const helper = require('./helper')
 
 exports.matchTextLengthToArea = (speech, terms) => {
@@ -11,30 +13,65 @@ let textAreas = terms.map(t, i => {
    let textArea = height * width
    return { 
     text: t,
-    id: i,
+    textId: i,
     area: textArea
    }
 })
 
-  speech.forEach(s => {
-    let area = helper.getArea(a)
-    console.log("Area")
-    console.log(area)
-    terms.forEach(t => {
-      console.log("Term string")
-      console.log(t.length)
-    })
-  }
-  return arr of objects with [{ coords: [], terms: "" }]
+let speechAreas = speech.map(s, i => {
+  let area = helper.getArea(a)
+  s.area = area
+  s.speechId = i
+  return s
+})
 
-  let combinedData = speech.map((s, i) => {
+let arr = []
+arr.push(textAreas)
+arr.push(speechAreas)
+let flat = _.flatten(arr)
+let vectors = flat.map(f => {
+   return [f.area]
+})
+
+let clusterResult = cluster(vectors)
+let matchResult = matchBack(speechAreas, textAreas, clusterResult)
+
+function matchBack(speechAreas, textAreas, clusterResult){
+  let arr = []
+  speechAreas.forEach(s => {
+    textAreas.forEach(t => {
+      clusterResult.forEach(c => {
+          c.cluster.forEach(cl => {
+             if (cl === t.area) {
+                 c.t = t
+             }
+             if (cl === s.area) {
+                 c.s = s
+             }
+          }
+      })
+    })
+  })
+  return clusterResult
+}
+  
+  
+
+//return arr of objects with [{ coords: [], terms: "" }]
+
+function cluster(vectors){
+  kmeans.clusterize(vectors, {k: speech.length }, (err,res) => {
+    if (err) console.error(err)
+    else return res
+  })
+}
+   
+ let combinedData = fullArr.map((s, i) => {
     return {
       coordinates: s.coords,
       terms: s.terms,
-      width: helper.getWidth(d),
-      topLeft: helper.getTopLeft(d)
+      width: helper.getWidth(s.coords),
+      topLeft: helper.getTopLeft(s.coords)
     }
   })
-
-
 }
